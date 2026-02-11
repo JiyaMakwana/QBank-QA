2559ec8b0a717270a0706eef13ef5794ff2b4e02
JiyaMakwana <jiyaedu7718@gmail.com>
2026-02-11 14:18:32 +0530

    Update App.jsx

--- React-learning/src/App.jsx
+++ React-learning/src/App.jsx
@@ -9,17 +9,28 @@ function App() {
   const age=21;
   const city="Ahmedabad";
   const skills = ["HTML", "CSS", "JavaScript"];
+  const isLoggedIn=true;
+  const users = [
+    { id: 1, name: "Jiya", age: 21 },
+    { id: 2, name: "Aman", age: 22 },
+    { id: 3, name: "Neha", age: 20 }
+  ];
+
 
   return (
     <div>
-    <h1>Hello {name} 🚀</h1>
-    <p>My age is {age}.</p>
-    <p>Next year i will be {age + 1}.</p>
-    <p>I live in {city}.</p>
-    <ul type="square">
-      {skills.map(skill => <li>{skill}</li>)}
-    </ul>
-    <button>Click Me</button>
+      {isLoggedIn ? <h1>Welcome Back</h1> : <h1>Please Login</h1>}
+      <h1> {name}🚀</h1>
+      <p>My age is {age}.</p>
+      <p>Next year i will be {age + 1}.</p>
+      <p>I live in {city}.</p>
+      <ul type="square">
+        {skills.map(skill => <li>{skill}</li>)}
+      </ul>
+      <p>
+        {users.map((user) => <p>{user.name} - {user.age} years old</p>)}
+      </p>
+      <button>Click Me</button>
   </div>
   )
 }
